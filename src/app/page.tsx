515c576e6ff253adea98e860659a75176082173d
raymasahil <sahil@teque7.com>
2024-04-27 12:14:00 +0530

    Update page.tsx
    home page text update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,8 +44,8 @@ export default function Home() {
               {/* <p className='text-sm mb-2 text-pink-500'>About Us</p> */}
 
               
-                <h1 className='text-5xl font-bold text-purple-900'>Dr. Surabhi Vegadd</h1> 
-                <p className='mb-6 text-sm text-pink-500 pt-2'>Leading Gynecologist in BHUJ, Kutch</p>
+                <h1 className='text-5xl font-bold text-purple-900'>Dr. Surabhi Vegad</h1> 
+                <p className='mb-6 text-sm text-pink-500 pt-2'>Leading Gynecologist in Bhuj, Kutch</p>
                 <div className='flex justify-center heading-title'>
                   <span>
                     <Image className='mx-auto' src="/Images/Group-98.png" alt="" width={40} height={40} />
